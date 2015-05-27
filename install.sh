718508c0500cc9c454193aa83238233c3e4835fe
psath <paul.sathre@gmail.com>
2015-05-26 20:06:02 -0400

    Added portions of the install script necessary to build the CU2CL plugin after building Clang 3.2

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##Download LLVM/CLang revision 159674 -- REQUIRES SVN
+## Download LLVM/CLang revision 159674 -- REQUIRES SVN
 svn checkout -r 159674 http://llvm.org/svn/llvm-project/llvm/trunk llvm
 cd llvm/tools
 svn checkout -r 159674 http://llvm.org/svn/llvm-project/cfe/trunk clang
@@ -15,5 +15,21 @@ cmake -DCMAKE_BUILD_TYPE=Release ../llvm
 ## adjust the argument to -j to suit available cores for speed
 make -j2
 
-cd ..
-#done
+## Repair a few unlinked header files in this Clang Revision
+ln -s `pwd`/tools/clang/include/clang/Basic/AttrList.inc ../llvm/tools/clang/include/clang/Basic/AttrList.inc
+ln -s `pwd`/tools/clang/include/clang/Basic/DiagnosticCommonKinds.inc ../llvm/tools/clang/include/clang/Basic/DiagnosticCommonKinds.inc
+ln -s `pwd`/tools/clang/include/clang/AST/DeclNodes.inc ../llvm/tools/clang/include/clang/AST/DeclNodes.inc
+ln -s `pwd`/tools/clang/include/clang/AST/StmtNodes.inc ../llvm/tools/clang/include/clang/AST/StmtNodes.inc
+ln -s `pwd`/tools/clang/include/clang/AST/Attrs.inc ../llvm/tools/clang/include/clang/AST/Attrs.inc
+
+## Make a directory for the cu2cl shared library.
+mkdir ../cu2cl-build
+cd ../cu2cl-build
+
+## Use CMake to configure the build
+cmake -DCU2CL_PATH_TO_LLVM_SRC=../llvm -DCU2CL_PATH_TO_CLANG_SRC=../llvm/tools/clang -DCU2CL_PATH_TO_LLVM_BUILD=../llvm-build -DCU2CL_PATH_TO_CLANG_BUILD=../llvm-build -DCMAKE_BUILD_TYPE=Release ../
+
+## Then build CU2CL
+make
+
+## done
